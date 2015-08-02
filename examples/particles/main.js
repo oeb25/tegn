77d6283bdef3fe11ca355d4218c102f2b8ba7bb0
Oliver Bøving <lvrmlbvng@gmail.com>
2015-08-02 13:58:57 +0200

    Added particles and text examples

---
+++ examples/particles/main.js
@@ -0,0 +1,64 @@
+import tegn, { init } from '../../src';
+
+const ctx = init(1280, 720);
+
+let particles = [];
+
+let tick = 0;
+
+const Particle = (x, y) => {
+  const num = Math.random();
+
+  const dir = num * 100;
+  const speed = num * 3 + 1;
+
+  return {
+    x, y,
+
+    fill: '#aae',
+
+    width: 2 * speed,
+    height: 2 * speed,
+
+    velocity: {
+      x: Math.cos(dir) * speed,
+      y: Math.sin(dir) * speed,
+    }
+  };
+}
+
+const loop = () => {
+  requestAnimationFrame(loop);
+
+  if (tick++ % 1 === 0) {
+    particles
+      //.filter(p => p.y < 720)
+      .forEach(p => {
+        p.x = p.x + p.velocity.x;
+        p.y = p.y + p.velocity.y;
+        p.velocity = {
+          x: p.velocity.x * 0.999,
+          y: p.velocity.y + 0.004
+        };
+      });
+
+    particles.push(Particle(1280 / 2, 720 / 2));
+
+    if (tick % 10 === 0) {
+      console.log(particles.length);
+    }
+  }
+
+  tegn(ctx, {
+
+    fill: '#333',
+
+    width: 1280,
+    height: 720,
+
+    children: particles
+
+  });
+};
+
+loop();
